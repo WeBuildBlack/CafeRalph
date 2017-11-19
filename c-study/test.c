f67d03201a5d14a4969709d1521e116f0befc889
jackson- <jackson.t.devin@gmail.com>
2017-11-19 17:14:09 -0500

    init commit

---
+++ c-study/test.c
@@ -0,0 +1,6 @@
+# include <stdio.h>
+/* count characters in input; 2nd version */
+int main()
+{
+	printf("%d", 9/2);
+}
---
+++ test-proj/myScripts.js
@@ -0,0 +1,5 @@
+var bullshit = 0
+
+console.log(bullshit)
+bullshit+=1
+console.log(bullshit)
